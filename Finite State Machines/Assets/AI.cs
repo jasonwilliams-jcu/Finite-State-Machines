a9e01ba56f056019a7a34975176776e44a29df17
jasonwilliams-jcu <jason.williams1@my.jcu.edu.au>
2023-04-14 16:17:35 +1000

    Update AI.cs syntax

--- Finite State Machines/Assets/AI.cs
+++ Finite State Machines/Assets/AI.cs
@@ -1,21 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using unityEngine.AI;
+using UnityEngine.AI;
 
 public class AI : MonoBehaviour
 {
 
     NavMeshAgent agent;
     Animator anim;
-    public Transform plaer;
+    public Transform player;
     State currentState;
     // Start is called before the first frame update
     void Start()
     {
         agent = this.GetComponent<NavMeshAgent>();
-        anim = this.GetComponent<NavMeshAgent>();
-        currentState = new Idle(this.gameobject, agent, anim, player);
+        anim = this.GetComponent<Animator>();
+        currentState = new Idle(this.gameObject, agent, anim, player);
     }
 
     // Update is called once per frame
